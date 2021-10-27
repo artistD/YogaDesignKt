7c4dbcf7cb457ba7bacdf83fc267282fe6acada9
dohagtae <willd88@naver.com>
2021-10-27 16:45:28 +0900

    second commit

---
+++ app/src/main/java/com/will_d/yogadesignkt/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.will_d.yogadesignkt
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class HomeFragment(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/will_d/yogadesignkt/MainActivity.kt
+++ app/src/main/java/com/will_d/yogadesignkt/MainActivity.kt
@@ -2,8 +2,12 @@ package com.will_d.yogadesignkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    val bnv:BottomNavigationView by lazy { findViewById(R.id.bnv) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
